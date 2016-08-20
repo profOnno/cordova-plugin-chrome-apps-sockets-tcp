36513933adf83654cf3a1f81dca7dbc01cebc06d
Menno Vossen <nemno@kuopzi.nl>
2016-08-20 21:54:41 +0200

    create, connect, wsPipeTcp works... needs cleanup and socket.io

--- src/browser/ChromeSocketsTcpProxy.js
+++ src/browser/ChromeSocketsTcpProxy.js
@@ -3,7 +3,11 @@
  */
 var postAction = function postAction(actionObject, callback) {
 
-    var xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest(),
+        data = JSON.stringify(actionObject);
+
+    console.log("proxy>");
+    console.log(data);
 
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) { 
@@ -13,20 +17,47 @@ var postAction = function postAction(actionObject, callback) {
 
     xhttp.open("POST", "http://localhost:8001/", true); 
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(actionObject));
+    xhttp.send(data);
 };
- 
-var ws = null;
+
+var log = function () {
+    // should do something with the args
+    // so "bla: %s, %s", x, y works
+    var prefix = "PROXY> ";
+
+    var myAr = Array.prototype.slice.call(arguments);
+    myAr[0] = prefix + myAr[0];
+    //doesn't have the stack... line number...
+    console.log.apply(console, myAr);
+};
+
+var ws = {},
+    listenerObj = {},
+    eventListeners = [],
+    successHandler, errorHandler;
+  //  menno = onReceive; //onReceive not defined is exported from www to frontend?
+
 var ChromeSocketsTcp = {
-
+    readyToRead: function (win, fail, args, env) {
+        console.log("PROXY: hmmm data?");
+    },
+   
     create: function (success, error, options) {
-        
-       
 
-        console.log("create called");
-        console.log(JSON.stringify(options));
-        postAction({ action: "create" }, function (res){
-            return success(res);
+        log("create called");
+        log(JSON.stringify(options));
+        postAction({ action: "create" }, function (res) {
+            log(res);
+            res = JSON.parse(res);
+            log(res);
+            if (res.result === "ok") {
+                log("res.result === \"ok\"");
+                log("aarg:");
+                log(JSON.stringify(res.data));
+                success(res.data.socketId); // TODO is weird... cause data should be the object already
+            } else {
+                error(res.result);
+            }
         });
 
             // start proxy ws-socket
@@ -38,19 +69,163 @@ var ChromeSocketsTcp = {
 
     },
 
+    registerReceiveEvents: function(success, error, options) {
+        /*
+        console.log("CSTP: registerReceiveEvents");
+        
+        console.log(arguments[0].toString()); //onSuccess
+        console.log(arguments[1]); //onError
+        console.log(arguments[2]); //array arguments...
+        //should be an id??
+        */
+        
+
+        // function is correct... but the options???
+        // like set keepCallback...
+        successHandler = function (data) {
+            var callbackOptions = { 
+        //            callbackId: "menno", 
+                    keepCallback: true
+                };
+            success(data, callbackOptions) ;
+        };
+        errorHandler = error;
+       
+/*        window.addEventListener('onReceive', function () {
+            success("hello");
+        }, false);
+        */
+        
+        //
+        // asumes readyToRead on backend
+        // win = function(info, data) ...
+        // seems to be the callback
+        // 2nd argument = [];//filled when listener added?
+        // android has socketId there
+        // is called on startup...
+        // 
+        // in test this is called... so do we in the index.html 
+        // chrome.sockets.tcp.onReceiveError.addListener(receiveErrorListener);
+        // chrome.sockets.tcp.onReceive.addListener(receiveListener);
+
+        /*
+        listenerObj.addEventListener('receive');
+        listenerObj.addEventListener('receiveError');
+        eventListeners.push(obj);
+        */
+        return ""; //cordova.js:910
+    },
+
+/*    readyToRead: function () {
+        console.log("CSTP readyToRead");
+        console.log(arguments[0]);
+        console.log(arguments[1]);
+    },
+
+    */
+    onReceive: function () {
+        console.log("onReceive triggerd");
+        console.log(JSON.stringify(arguments));
+    },
+
     connect: function (success, error, options) {
-        console.log("connect called");
-        console.log(JSON.stringify(options));
-        return success();
+        log("connect called");
+        log(JSON.stringify(options));
+
+        //var socketId = options.data[0];
+        // don't know why options has socketId in object???
+        postAction({ action: "connect", data: options  }, function (res) {
+            log("back from postAction connect");
+            log(res); 
+            res = JSON.parse(res);
+
+            if (res.result === "ok") {
+                // make a websocket
+                log("socketId:", socketId);
+                var myws = ws[socketId] = new WebSocket("ws://127.0.0.1:8001/"+socketId); // the ws port and host, not the tcp endpoint
+
+                myws.binaryType = 'arraybuffer';
+                myws.onclose = function (evt) {
+                    // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
+                    log("websocket close event code:", evt.code);
+                };
+                myws.onmessage = function (evt) {
+                    log("got websocket message, got to relay");
+                    log(evt.data);
+                    // web js... uses dispatchEvent??
+
+                    // this will throw a TypeError;
+                    // cordova.js:290 expects the callback (successHandler) to be in the callbacks object
+                    // cordova.js:934 should register the callback
+                    // it is not registered there... so huh...
+                    // maybe buffer data here... call a existing event trigger...
+                    try {
+                        //now only called once... seems.. to invoke the send response instead of or own one
+                        setTimeout(function () {
+                            successHandler(evt.data); //1 should be SOCKET IDDD TODO
+                        },100);
+                    } catch (e) {
+                        //console.log("type error");
+                    }
+                   //this calls the function in the event handler...
+                   //but it should return something??
+
+
+                    //console.log(JSON.stringify(ChromeSocketsTcp.onReceive));
+                    //var onReceive = new Event('onReceive');
+
+                    //window.dispatchEvent('onReceive', evt.data);
+                };
+                myws.onopen = function () {
+                    //myws.send("hello you there...");
+                    log("websocket open...");
+                    //myws.send(new Uint8Array([0x10, 0x02, 0x31, 0x7e, 0x94, 0x10, 0x03]));
+                    
+                    ws[socketId] = myws; // add to sockets object
+                    
+                    return success(1); // TODO get the right result codes, result code negative is error... pos is ok
+                };
+            } else {
+                return error(-1);
+            }
+        });
     },
 
-    write: function () {
-        console.log("write called");
-    },
+    send: function (success, error, options) {
+        log("send called");
+        console.log(options); // should be array with socketId and data;
+        var socketId = options[0];
 
+        ws[socketId].send(options[1]);
+        /*
+        console.log(arguments);
+        console.log(options); // should be array with socketId and data;
+        console.log(options[1]); // should be array with socketId and data;
+        options[1] = b64.fromByteArray(new Uint8Array(options[1]));
+        console.log(options[1]); // should be array with socketId and data;
+        postAction( { action: "send", data: options }, function (res) {
+            // whatdafuck is bytesSent
+            // seems to be a resultCode: 0 
+            // parse res
+            res = JSON.parse(res);
+            if (res.result === "ok") {
+                var retVal = res.cnt;
+            } else {
+                error(res.result);
+            }
+            return success(res); 
+        });
+        */
+        // bytes send??
+        return success(10); //TODO whats the return value
+
+}
+
+/*
     read: function () {
-        console.log("read called");
+        log("read called");
     }
+    */
 };
 
 module.exports = ChromeSocketsTcp;
--- src/browser/ProxyServerHook.js
+++ src/browser/ProxyServerHook.js
@@ -8,13 +8,22 @@
 module.exports = function (context) {
     //var http  = require('http');
     var express = require('express'),
+        app = express(),
         bodyParser = require('body-parser'),
-        app = express();
+        server = require('http').createServer(),
+        randid = require('random-strings'),
+        Socket = require('net').Socket,
+        b64 = require('base64-js'),
+        websocket = require('websocket-stream'),
+        DuplexStream = require('stream').Duplex,
+        wsSockets = {}, //holds websockets to frontend
+        tcpSockets = {}, //holds tcp sockets to backend 
+        cnt = 0;
 
     //CORS middleware
     var allowCrossDomain = function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*'); //localhost???
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Methods', 'GET,POST');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
 
         next();
@@ -22,46 +31,41 @@ module.exports = function (context) {
 
     console.log("starting ProxyServerHook");
 
+    // intermediate websocket test 
+    // var iWss = new ws({ port: 8066 }); //this works
+    // var iWss = websocket.createServer({ port: 8066 }); //this also works
+   /* 
+    var iWss = websocket.createServer({ server: server }); 
+    
+    iWss.on('connection', function (iws) {
+        iws.on('message', function (msg) {
+            console.log('msg:',msg);
+        });
+        iws.on('message', function (msg) {
+            console.log('msg:',msg);
+        });
+    });
+
+    iWss.on('error', function (error) {
+        console.log("iwss error:",JSON.stringify(error));
+    });
+    */
+    // END intermediate websocket test 
+
     app.use(allowCrossDomain);
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
-    app.get('/', function(req, res) {
+    /*app.get('/', function(req, res) {
         //res.writeHead(200,{"Content-Type": "text/html"});
-/*        var msg = "You should not be sending an html request...<br>try something like this:<br><br>";
-            msg += "<code>curl -H \"Content-Type: application/json\" -X POST -d '{\"action\": \"create\"}' http://localhost:8001</code>";
-            */
-        var msg = '<!DOCTYPE html>\n';
-            msg += '<html>\n';
-        msg += '<body>\n';
-
-        msg += '<h2>AJAX</h2>\n';
-
-        msg += '<button type="button" onclick="postit()">postit</button>\n';
-
-        msg += '<p id="demo"></p>\n';
-         
-        msg += '<script>\n';
-        msg += 'function postit() {\n';
-              msg += 'var xhttp = new XMLHttpRequest();\n';
-                msg += 'xhttp.onreadystatechange = function() {\n';
-                        msg += 'if (xhttp.readyState == 4 && xhttp.status == 200) {\n';
-                                  msg += 'document.getElementById("demo").innerHTML = xhttp.responseText;\n';
-                                msg += '  }\n';
-              msg += '};\n';
-             msg += ' xhttp.open("POST", "http://localhost:8001/", true);\n'; // TODO add cors for 127.0.0.1?
-              msg += 'xhttp.setRequestHeader("Content-type", "application/json");\n';
-              msg += 'xhttp.send(JSON.stringify({ action: "create" }));\n';
-        msg += '}\n';
-        msg += '</script>\n';
-        msg += '</body>\n';
-        msg += '</html>\n';
-        res.send(msg);
+       res.sendFile(__dirname + '/www/index.html');
     });
+*/
+    app.use(express.static(__dirname + "/www"));
 
     app.post('/', function (req,res) {
-        console.log('got post action');
-        console.log(JSON.stringify(req.body, null, 2));
+ //       console.log('got post action');
+  //      console.log(JSON.stringify(req.body, null, 2));
         
         //dosomething
         
@@ -69,15 +73,88 @@ module.exports = function (context) {
 
         switch (req.body.action) {
             case "create":
-                console.log("create request");
-                res.write('{"result": "ok"}');
+                (function () {
+//                    console.log("create request");
+                    // TODO try block..
+                    var sid = randid.alphaNumMixed(16);
+ //                   console.log("sid:", sid);
+                    
+                    var client = new Socket();
+
+                    tcpSockets[sid] = client;
+    /*                
+                    app.sockets[sid].connect({port: 3000, host: "192.168.1.101"}, function () {
+                        console.log("connected");
+                        app.sockets[sid].write("hellejjjj lujaaaa");
+                    });
+                    */
+                    
+                    
+                    // rewrite object to chrome api spec
+                    var result = { 
+                            result: "ok",
+                            data: {
+                                socketId : sid 
+                            }
+                    };
+
+                    res.write(JSON.stringify(result));
+                    res.end();
+                }());
                 break;
             
             case "connect":
-                console.log("create request");
-                console.log("connecting to ip: %s", req.body.ip);
-                console.log("connecting to port: %s", req.body.port);
-                res.write('{"result": "ok"}');
+                (function () {
+   //                 console.log("connect");
+    //                console.log("req.body:");
+
+                    var sRef = req.body.data[0],
+                        ip = req.body.data[1],
+                        port = req.body.data[2];
+
+                    var client = tcpSockets[sRef];
+
+     //               console.log("sRef: %s, ip: %s, port: %s", sRef, ip, port);
+                    
+                    client.connect({ host: ip, port: port }, function () {
+      //                  console.log("connected"); 
+                        // now make a new websocket and bind it to this client.
+                        // websocket sever should not be started over here
+                        // merge the async.. this connect request and the new incomming connection..
+                        // so how do we do that
+       //                 console.log("ws url should add sRef:",sRef); 
+                        wsSockets[sRef] = websocket.createServer({ server: server, path: "/" + sRef }, function (stream) {
+                            client.pipe(stream);
+                            stream.pipe(client);
+                            //hmm works only from tcp to webclient
+                            //
+                        });
+
+                        //stream it 
+                        /*
+                        wsSockets[sRef].on('connection', function ( wsClient) {
+                            console.log("got websocket connection");
+                            wsClient.send("aarghihiehiaoij");
+                            wsClient.on('message', function (msg) {
+                                console.log('received:', msg);
+                            });
+
+                        });
+                        */
+                        /* for the stream stuff that doesn't work 
+                        {
+                            console.log("got stream data, trying to connect it");
+                            stream.pipe(client); //hope this work
+                        });
+                        */
+                        // TODO check if this async works... meanig result isn't send to early
+                        // should be in the websocket.on 'connection'
+                        var result = { result: "ok", data: 1 }; // data should be result code from underlying call... negative is error
+                        res.write(JSON.stringify(result));
+                        res.end();
+                    });
+
+                }());
                 break;
 
             default:
@@ -86,14 +163,14 @@ module.exports = function (context) {
         }
 
         
-        res.end();
+        //res.end(); // needed? not like this... some res.write are encapsulated in callbacks..
         
     });
 
-    app.listen(8001, function () {
+    server.on('request', app);
+    server.listen(8001, function () {
         console.log("Listening on port 8001...");
     });
 
-//    console.log(JSON.stringify(context));
 };
 
