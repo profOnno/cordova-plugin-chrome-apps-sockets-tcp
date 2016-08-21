3d00081e8e7409dce218d8e6bf33757a2086fbb0
Menno Vossen <nemno@kuopzi.nl>
2016-08-21 13:09:40 +0200

    does it work

--- src/browser/install_dep.js
+++ src/browser/install_dep.js
@@ -1,4 +1,20 @@
-var execFile = require('child_process').execFile;
+var execFile = require('child_process').execFile,
+    spawn = require('child_process').spawn;
 
 console.log("installing dependencies...");
-execFile('npm',['install']);
+/*
+execFile('npm', ['install'], { cwd: "src/browser" }, function (error, stdout, stderr) {
+    if (error) {
+        throw error;
+    }
+    console.log(stdout);
+});
+*/
+var npm = spawn('npm', ['install'], { cwd: "src/browser" });
+npm.stdout.pipe(process.stdout);
+npm.stderr.pipe(process.stderr);
+
+npm.on('close', function (code) {
+    console.log("done: ", code);
+});
+
